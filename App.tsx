020043196713dd49668df310d917d4464c86f297
Bowen Liu <bowenliu22@gmail.com>
2024-11-27 14:04:11 -0800

    init react native testApp

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ function App(): React.JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            screen and then come back to see your edits. We have an active working prototype that lets me change things on the go
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
